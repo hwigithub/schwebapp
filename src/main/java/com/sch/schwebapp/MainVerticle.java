1837cdee4efc7bca4e56d151d9ff6535170e5455
sch <hotoleanu.ciprian@gmail.com>
2018-04-20 23:55:48 +0300

    frontend side

--- src/main/java/com/sch/schwebapp/MainVerticle.java
+++ src/main/java/com/sch/schwebapp/MainVerticle.java
@@ -1,16 +1,19 @@
 package com.sch.schwebapp;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.StaticHandler;
 
 public class MainVerticle extends AbstractVerticle {
 
     @Override
     public void start() throws Exception {
-        vertx.createHttpServer().requestHandler(req -> {
-              req.response()
-                .putHeader("content-type", "text/plain")
-                .end("Hello from Vert.x!");
-            }).listen(8080);
+        Router router = Router.router(vertx);
+
+        router.route("/app/*").handler(StaticHandler.create().setCachingEnabled(false).setWebRoot("frontend"));
+
+        vertx.createHttpServer().requestHandler(router::accept).listen(8080);
+
         System.out.println("HTTP server started on port 8080");
     }
 }
